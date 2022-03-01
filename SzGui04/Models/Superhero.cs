3ade12ff77956d6b77ff3a15dc38dea88a355d64
hallgato <hallgato@LABOR215-02>
2022-03-01 13:58:00 +0100

    superhero done

--- SzGui04/Models/Superhero.cs
+++ SzGui04/Models/Superhero.cs
@@ -7,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace SzGui04.Models
 {
-    class Superhero: ObservableObject
+    public enum Alignment { good,bad,neutral}
+    public class Superhero: ObservableObject
     {
+        private int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { SetProperty(ref speed, value); }
+        }
+        private int power;
+        public int Power
+        {
+            get { return power; }
+            set { SetProperty(ref power, value); }
+        }
+        private int name;
+        public int Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+        private Alignment alignment;
+        public Alignment Alignment
+        {
+            get { return alignment; }
+            set { SetProperty(ref alignment, value); }
+        }
     }
 }
