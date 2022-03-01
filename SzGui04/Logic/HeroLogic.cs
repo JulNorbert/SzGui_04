f14a5aaef2876338a600708a71799fc4d5f41e94
hallgato <hallgato@LABOR215-13>
2022-03-01 15:01:46 +0100

    HeroLogic with interface done

---
+++ SzGui04/Logic/HeroLogic.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SzGui04.Models;
+
+namespace SzGui04.Logic
+{
+    public class HeroLogic : IHeroLogic
+    {
+        IList<Superhero> working;
+        IList<Superhero> atHome;
+        IMessenger messenger;
+
+        public HeroLogic(IMessenger messenger)
+        {
+            this.messenger = messenger;
+        }
+
+
+        public double AVGPower
+        {
+            get
+            {
+                return Math.Round(working.Count == 0 ? 0 : working.Average(x => x.Power), 2);
+            }
+        }
+        public double AVGSpeed
+        {
+
+            get
+            {
+                return Math.Round(working.Count == 0 ? 0 : working.Average(x => x.Speed), 2);
+            }
+        }
+
+        public void GoToWork(Superhero hero)
+        {
+            working.Add(hero.GetCopy());
+        }
+
+        public void GoHome(Superhero hero)
+        {
+            working.Remove(hero);
+        }
+
+        public void SetupCollections(IList<Superhero> working, IList<Superhero> atHome)
+        {
+            this.working = working;
+            this.atHome = atHome;
+        }
+    }
+}
--- SzGui04/ViewModels/MainWindowViewModel.cs
+++ SzGui04/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,45 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SzGui04.Logic;
+using SzGui04.Models;
 
 namespace SzGui04.ViewModels
 {
-    public class MainWindowViewModel:ObservableRecipient
+    public class MainWindowViewModel : ObservableRecipient
     {
+        IHeroLogic logic;
+        ObservableCollection<Superhero> working;
+        ObservableCollection<Superhero> atHome;
+
+        private Superhero selectedFromAtHome;
+
+        public Superhero SelectedFromAtHome
+        {
+            get { return selectedFromAtHome; }
+            set {
+                SetProperty(ref selectedFromAtHome, value);
+                    selectedFromAtHome = value;}
+        }
+
+        private Superhero selectedFromWorking;
+
+        public Superhero SelectedFromWorking
+        {
+            get { return selectedFromWorking; }
+            set { selectedFromWorking = value; }
+        }
+
+
+        public MainWindowViewModel(IHeroLogic logic)
+        {
+            this.logic = logic;
+
+            logic.SetupCollections(working, atHome);
+        }
     }
 }
